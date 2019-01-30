1c43fc2540b02d82d454b12bb3b06e5ebe6764d1
Eric Willroth <ewillroth@gmail.com>
2019-01-30 16:09:28 -0600

    add session

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const {json} = require('body-parser')
 const massive = require('massive')
-const app = express()
+const session = require("express-session");
 const cors = require('cors')
-const port = process.env.PORT
 const controller = require('./controller')
+const app = express()
+const port = process.env.PORT
 
 app.use(json())
 app.use(cors())
@@ -15,6 +16,17 @@ massive(process.env.CONNECTION_STRING)
 		app.set('db', db);
 		console.log('database connected');
 	})
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24
+		}
+	})
+);
 
 
 app.listen(port, ()=>console.log(`listening on ${port}`))
